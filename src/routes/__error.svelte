38664fd07b698805364abb42353a6784c57cce28
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2022-01-16 22:43:49 -0600

    Remove RSS hack from __error.svelte file

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -8,23 +8,8 @@
 
 
 <script>
-  import { currentPage } from '$lib/assets/js/store'
-  import { onMount } from 'svelte'
-
   export let status
   export let error
-
-  /**
-   * I'm not really sure why this is necessary; I think it must be
-   * a bug with the current version of SvelteKit. But for some reason,
-   * the initial router load of the RSS page always 404s. This just
-   * reloads the page, so it actually displays properly.
-  */
-  onMount(() => {
-    if ($currentPage.includes('rss.xml')) {
-      window.location.reload()
-    }
-  })
 </script>
 
 
