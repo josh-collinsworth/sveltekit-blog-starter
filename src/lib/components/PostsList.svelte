8fed0b4538ea1cf14b76ada17bbe215c7be98e02
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2022-01-23 09:37:46 -0600

    A11y improvements to PostsList component

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -6,18 +6,20 @@
 <ul class="posts-list">
   {#each posts as post}
     <li>
-      <a href="/blog/{post.slug}">
-        <img
+      <article>
+        <a href="/blog/{post.slug}">
+          <img
           src={post.coverImage}
           alt=""
           width={post.coverWidth}
           height={post.coverHeight}
           style="ratio: {post.coverWidth} / {post.coverHeight}"
-        />
-        <h2>
-          {post.title}
-        </h2>
-      </a>
+          />
+          <h2>
+            {post.title}
+          </h2>
+        </a>
+      </article>
 
       <p>{post.excerpt}</p>
     </li>
