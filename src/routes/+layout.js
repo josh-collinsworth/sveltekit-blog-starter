54a9b1ef96a48c0d559aaa16345ccce62f3c35ee
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2022-08-19 15:01:04 -0500

    Re-add RSS route inside load function

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -6,7 +6,7 @@ export const load = async ({ url }) => {
 		 * This fetch call is not used in this file, but the route won't be pre-rendered 
 		 * and routed properly unless it's called inside a `load` function. ¯\_(ツ)_/¯
 		 * */ 
-		// const rss = await fetch(`/api/rss.xml`)
+		const rss = await fetch(`${url.origin}/api/rss.xml`)
 
 		return {
 			path: url.pathname
