e233f72f93ee7d1f4611002938e0bebc53cbcf7a
Jason Kurian <JaKXz@users.noreply.github.com>
2022-10-16 21:33:37 -0400

    fix typos and add prerender.entries
    but still missing one last thing

--- src/routes/blog/category/page/[page]/+page.svelte
+++ src/routes/blog/category/page/[page]/+page.svelte
@@ -1,22 +1,20 @@
 <!-- Renders posts listed by category -->
 <script>
-	import PostsList from '$lib/components/PostsList.svelte'
-	import Pagination from '$lib/components/Pagination.svelte'
-	import { siteDescription } from '$lib/config'
+	import Pagination from '$lib/components/Pagination.svelte';
+	import PostsList from '$lib/components/PostsList.svelte';
+	import { postsPerPage, siteDescription } from '$lib/config';
 
-	export let data
-  const { page, totalPosts, posts } = data
+	export let data;
+	const { page, totalPosts, posts } = data;
 
-	$: lowerBound = (page * postsPerPage) - (postsPerPage - 1) || 1
-	$: upperBound = Math.min(page * postsPerPage, totalPosts)
+	$: lowerBound = page * postsPerPage - (postsPerPage - 1) || 1;
+	$: upperBound = Math.min(page * postsPerPage, totalPosts);
 </script>
-
 
 <svelte:head>
 	<title>Blog category - page {page}</title>
-	<meta data-key="description" name={siteDescription}>
+	<meta data-key="description" name={siteDescription} />
 </svelte:head>
-
 
 <!-- TODO: this is duplicated across multiple `+page.svelte` files -->
 {#if posts.length}
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-static'
-import { mdsvex } from 'mdsvex'
-import preprocess from 'svelte-preprocess'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
+import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,22 +14,28 @@ const config = {
 			scss: {
 				// Ensures Sass variables are always available inside component <style> blocks as vars.$variableDefinedInFile
 				prependData: `@use 'src/lib/assets/scss/vars';`
-			},
+			}
 		}),
 		mdsvex({
 			// The default mdsvex extension is .svx; this overrides that.
 			extensions: ['.md'],
 
 			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+		})
 	],
 
 	kit: {
-		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*',
+				'/blog/page/*',
+				'/blog/category/*',
+				'/blog/category/*/page/*',
+				'/blog/category/page/*'
+			]
+		},
+		adapter: adapter()
 	}
 };
 
