874c39bed8136a6595caeecc9d2fb5a6da5101e7
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2022-10-20 22:23:16 -0500

    Remove apparently no longer necessary call to RSS in +layout.js; fix CSS issue with .callout component.

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,10 +1,5 @@
 import { error } from '@sveltejs/kit'
 
-<<<<<<< Updated upstream
-export const prerender = true
-
-export const load = async ({ url }) => {
-=======
 // Ensures all pages under this layout (which is all of them) are statically prerendered at build time 
 export const prerender = true
 
@@ -12,14 +7,7 @@ export const prerender = true
 export const csr = true
 
 export const load = async ({ url, fetch }) => {
->>>>>>> Stashed changes
 	try {
-		/**
-		 * This fetch call is not used in this file, but the route won't be pre-rendered 
-		 * and routed properly unless it's called inside a `load` function. ¯\_(ツ)_/¯
-		 * */ 
-		// const rss = await fetch(`${url.origin}/api/rss.xml`)
-
 		return {
 			path: url.pathname
 		}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,12 @@
 <script>
 	import { siteTitle } from '$lib/config';
 
-<<<<<<< Updated upstream
 	export let data;
-=======
-export let data
-
-const { ReadMe } = data
->>>>>>> Stashed changes
 </script>
 
 <svelte:head>
 	<title>{siteTitle}</title>
 </svelte:head>
 
-<<<<<<< Updated upstream
 <!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything in this file. -->
 <svelte:component this={data.ReadMe} />
-=======
-
-<svelte:component this={ReadMe} />
-<!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything in this file. -->
->>>>>>> Stashed changes
