8d811a3ad40a23c7ced41346e8ecabf72f5b3aa8
Georg Hartmann <georg.hartmann@hey.com>
2022-11-20 15:44:33 +0000

    Exclude posts with draft = true

--- src/lib/assets/js/fetchPosts.js
+++ src/lib/assets/js/fetchPosts.js
@@ -10,7 +10,9 @@ const fetchPosts = async ({ offset = 0, limit = postsPerPage, category = '' } = 
 		})
 	)
 
-	let sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date))
+	let publishedPosts = posts.filter(post => !post.draft)
+
+	let sortedPosts = publishedPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
 	
 	if (category) {
     sortedPosts = sortedPosts.filter(post => post.categories.includes(category))
